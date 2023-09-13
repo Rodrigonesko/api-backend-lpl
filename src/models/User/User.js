ca6614ccab980fda5be6481fa4c42b4fc475e5f8
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2023-09-13 14:46:22 -0300

    alterando user

--- src/models/User/User.js
+++ src/models/User/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const userScheema = mongoose.Schema({
+const userScheema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
