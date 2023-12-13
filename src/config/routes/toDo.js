821ad21aff565e3b1cf99e4ee621207f98c2ad27
Leonardo <leonardoarrudalonque@outlook.com>
2023-12-13 15:04:53 -0300

    Update ToDo, inventario, admissaoDemissao, routes

--- src/config/routes/toDo.js
+++ src/config/routes/toDo.js
@@ -6,7 +6,5 @@ const auth = require('../../middlewares/auth')
 //Crie as rotas
 
 router.post('/newToDo', auth, toDoController.newToDo)
-router.put('/update', toDoController.updateVacationTable)
-router.get('/filter', toDoController.getFeriasByFilter)
 
 module.exports = router
