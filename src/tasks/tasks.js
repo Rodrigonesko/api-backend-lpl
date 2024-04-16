39a5472c3e022e61da383b06d7f642233ba5b7e9
Rodrigo Onesko Dias <rodrigo_onesko@hotmai.com>
2024-04-16 16:57:25 -0300

    alterando service da sindicance para uma classe

--- src/tasks/tasks.js
+++ src/tasks/tasks.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const moment = require('moment');
 const avaliacoesService = require('../services/avaliacaoPerformanceIndividual.service');
+const sindicanciaService = require('../services/sindicancia.service');
 
 cron.schedule('0 8 * * 1', async () => {
 
@@ -18,7 +19,7 @@ cron.schedule('0 8 * * 1', async () => {
 // Para executar primeiro dia do mês às 8h
 
 cron.schedule('0 8 1 * *', async () => {
-    
+
     const dataInicio = moment().subtract(1, 'months').startOf('month').format('YYYY-MM-DD');
     const dataFim = moment().subtract(1, 'months').endOf('month').format('YYYY-MM-DD');
 
@@ -30,6 +31,13 @@ cron.schedule('0 8 1 * *', async () => {
     timezone: 'America/Sao_Paulo'
 });
 
+
+cron.schedule('0 0 * * *', async () => {
+    const demandas = await sindicanciaService.conferirDatasBradesco();
+    console.log(demandas);
+}, {
+    timezone: 'America/Sao_Paulo'
+})
 // cron.schedule('30 * * * * *', () => {
 //     // Sua lógica aqui
 //     console.log('Esta função é executada no segundo 30 de cada minuto.');
